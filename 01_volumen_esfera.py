f9ce00204966a3c2d650f65841273d2da52b2a34
noracamacho <ingnorac@gmail.com>
2024-04-28 16:08:52 -0600

    Change: pi from math method

--- 01_volumen_esfera.py
+++ 01_volumen_esfera.py
@@ -5,7 +5,9 @@ import math
 def calculateVolume(radio):
     return 4/3 * math.pi * math.pow(radio, 3)
 
-print(calculateVolume(2))
+radio = int(input('Ingresa el radio de la esfera: '))
+
+print(f'El volumen de la esfera con radio {radio} es {round(calculateVolume(radio), 2)} cm\xB3')
 
 # -------------
 
