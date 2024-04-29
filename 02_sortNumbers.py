0a5ba0844aca516564af3b258accede458b1a782
noracamacho <ingnorac@gmail.com>
2024-04-29 13:40:14 -0600

    Change: optimizing code

--- 02_sortNumbers.py
+++ 02_sortNumbers.py
@@ -1,13 +1,29 @@
 # Ordene 5 números de menor a mayor
 
-def sortNumbers(num):
-    for i in range(0, len(num) - 1):
-        for j in range(0, len(num) - 1):
-            if(num[j] > num[j + 1]):
-                aux = num[j]
-                num[j] = num[j + 1]
-                num[j + 1] = aux
-    return num
+def isFloat(element):
+    if element % 1 == 0:
+        return int(element)
+    return element
 
-print(sortNumbers([10, 3, 8, 2, 1]))
+def sortNumbers(numbers):
+    numbers.sort()
+    return list(map(isFloat, numbers))
 
+inputNumbers = []
+for num in range(0, 5):
+    usr = float(input('Ingresa un número: '))
+    inputNumbers.append(usr)
+
+print(sortNumbers(inputNumbers))
+
+
+
+
+# def sortNumbers(num):
+#     # for i in range(0, len(num) - 1):
+#     #     for j in range(0, len(num) - 1):
+#     #         if(num[j] > num[j + 1]):
+#     #             aux = num[j]
+#     #             num[j] = num[j + 1]
+#     #             num[j + 1] = aux
+#     return num
